2f8017c25a0d95b83d57c5c0b6a7c7a957ff6a19
Thach <ducthach.1403@gmail.com>
2020-03-21 09:19:16 +0700

    update

--- DownLoadAnhVoz.py
+++ DownLoadAnhVoz.py
@@ -15,14 +15,15 @@ if (tam):
 	os.mkdir('Image')
 soup=BeautifulSoup(br.text,'lxml')
 link=os.path.join(os.getcwd(),"Image")
-lastpage=soup.find('a',{"class": "pageNavSimple-el pageNavSimple-el--last"})
-lastpage=lastpage.get('href')
-number=lastpage.find('page-')+5
-numberpage=lastpage[number::]
-
-for k in range(1,int(numberpage)+1):
+lastpage=''
+k=0
+while True: 
+	k+=1
 	linkpage='https://next.voz.vn/t/no-sex-test-dang-anh-nextvoz.613/page-'+str(k)
 	br=s.get(linkpage)
+	lastpage=linkpage
+	if (br.url!=lastpage) and (k!=1):
+		break
 	soup=BeautifulSoup(br.text,'lxml')
 	print(br.url)
 	for i in soup.find_all('img'):
